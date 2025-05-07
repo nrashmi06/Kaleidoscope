88c4779ab00d449f93c4e17ac54c8775b4deb899
RAJAY04 <nnm22ad041@nmamit.in>
2025-05-07 23:37:34 +0530

    fix: enhanced error reponse of wrong http methods

--- backend/src/main/java/com/kaleidoscope/backend/shared/exception/CustomExceptionHandler.java
+++ backend/src/main/java/com/kaleidoscope/backend/shared/exception/CustomExceptionHandler.java
@@ -4,11 +4,9 @@ import com.kaleidoscope.backend.shared.exception.Image.ImageStorageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.multipart.MultipartException;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 @Component("sharedExceptionHandler")
@@ -16,5 +14,16 @@ public class CustomExceptionHandler {
     @ExceptionHandler(ImageStorageException.class)
     public ResponseEntity<String> handleImageStorageException(ImageStorageException ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleMethodNotSupported(HttpRequestMethodNotSupportedException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.METHOD_NOT_ALLOWED.value(),
+                "Method not allowed: " + ex.getMethod() + " is not supported for this endpoint. Supported methods are: " +
+                        String.join(", ", ex.getSupportedMethods()),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(error, HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
--- backend/src/main/java/com/kaleidoscope/backend/shared/exception/ErrorResponse.java
+++ backend/src/main/java/com/kaleidoscope/backend/shared/exception/ErrorResponse.java
@@ -1,4 +1,12 @@
 package com.kaleidoscope.backend.shared.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ErrorResponse {
+    private int status;
+    private String message;
+    private long timestamp;
 }
